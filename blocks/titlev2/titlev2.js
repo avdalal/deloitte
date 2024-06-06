3fb2e0ff41198b79efc8d59b873e5106ac35f3c0
Avi Dalal <avdalal@deloitte.co.il>
2024-06-06 09:27:56 +0300

    dynamicButton

--- blocks/titlev2/titlev2.js
+++
@@ -1,8 +0,0 @@
-export default function decorate(block) {
-    console.log("currBlock", currBlock);
-    block.innerHTML = `
-      <div class="title">
-        <h1>${currBlock.innerHTML}</h1>
-      </div>
-    `;
-  }
