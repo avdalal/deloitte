5b1c0544312d2b902cdae1da430d257ad8cfa44d
Avi Dalal <avdalal@deloitte.co.il>
2024-06-06 17:04:09 +0300

    add component

--- blocks/customButton/customButton.js
+++ blocks/customButton/customButton.js
@@ -2,21 +2,6 @@ export default function decorate(block) {
     // Find all p elements in the block
     const  [buttonText, buttonColor, buttonRadius, buttonTextSize, buttonTextStyle, href, onclick] = [...block.querySelectorAll('p')].map(p => p.textContent);
 
-    // Create a new button element
-    const button = document.createElement('a'); 
-
-    // Set the button's properties based on the p elements
-    button.textContent = buttonText;
-    button.style.backgroundColor = buttonColor;
-    button.style.borderRadius = buttonRadius;
-    button.style.fontSize = buttonTextSize;
-    console.log(href);
-    //check if href is undefined
-    if(typeof(href) != "undefined") {
-        button.href = href;
-    } else {
-        button.href = 'javascript:void(0)';
-    }
 
     // Remove the original p elements from the DOM
     [...block.querySelectorAll('p')].forEach(p => p.remove());
