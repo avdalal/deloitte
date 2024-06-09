d859a9b4eb122d71ca55fd1a86afc810ff198fa8
Avi Dalal <avdalal@deloitte.co.il>
2024-06-09 13:13:46 +0300

    add component

--- blocks/imageslider/imageslider.js
+++ blocks/imageslider/imageslider.js
@@ -1,9 +1,22 @@
-export default function decorate(block) {
-    const bgs = block.children;
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
 
-    for (let i = 0; i < bgs.length; i++) {
-        const bg = bgs[i];
-        bgs[i].className = `slide-${i}`;
+export default function decorate(block) {
+    const slides = block.children;
+
+    for (let i = 0; i < slides.length; i++) {
+        const slide = slides[i];
+        slide.className = `slide-${i}`;
+
+        [...slide.children].forEach((div) => {
+            if (div.children.length === 1 && div.querySelector('picture')) div.className = 'slide-image';
+            else div.className = 'slide-body';
+        });
+
+        slide.querySelectorAll('picture > img').forEach((img) => {
+            const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+            moveInstrumentation(img, optimizedPic.querySelector('img'));
+            img.closest('picture').replaceWith(optimizedPic);
+        });
     }
-   
 }
