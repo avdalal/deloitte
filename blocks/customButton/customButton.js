0c633c4afaf351099c143f34880ba52b3ab166b6
Avi Dalal <avdalal@deloitte.co.il>
2024-06-09 09:00:39 +0300

    add component

--- blocks/customButton/customButton.js
+++ blocks/customButton/customButton.js
@@ -11,30 +11,24 @@ export default function decorate(block) {
     button.style.borderRadius = buttonRadius.textContent;
     button.style.fontSize = buttonTextSize.textContent;
     button.style.fontStyle = buttonTextStyle.textContent;
-    button.onclick = onclick.textContent;
-    console.log(href.textContent);
-    //check if href is undefined
-    if(typeof(href) != "undefined") {
-        button.href = href.textContent;
-    } else {
-        button.href = 'javascript:void(0)';
-    }
+
+    // Check if href is not undefined before assigning
+    button.href = href ? href.textContent : 'javascript:void(0)';
+
+    // Check if onclick is not undefined before assigning
+    button.onclick = onclick ? onclick.textContent : null;
 
     // Create a div element
     const div = document.createElement('div');
-    // Create a div element
-    const p = document.createElement('p');
 
     // Add a class to the div
     div.classList.add('button-container');
 
     // Append the button to the div
-    p.appendChild(button);
-    // Append the button to the div
-    div.appendChild(p);
+    div.appendChild(button);
 
     // Remove the original p elements from the DOM
-    [...block.children].forEach(div => div.remove());
+    [...block.children].forEach(child => child.remove());
 
     // Append the div to the block
     block.appendChild(div);
