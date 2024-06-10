59c634c51bb61362d112e613ba6f93acf34cc9ca
Avi Dalal <avdalal@deloitte.co.il>
2024-06-10 09:15:05 +0300

    add component

--- blocks/imageslider/imageslider.js
+++ blocks/imageslider/imageslider.js
@@ -1,71 +1,67 @@
 export default function decorate(block) {
     const slides = Array.from(block.children);
-    const ul = document.createElement('ul');
-    ul.className = 'imageslider';
-  
+
     slides.forEach((slide, i) => {
-      const li = document.createElement('li');
-      li.className = `imageslider__slide ${i === 0 ? ' imageslider__slide--active' : ''}`;
-      li.id = `slide-${i}`;
-  
-      while (slide.firstElementChild) li.append(slide.firstElementChild);
-  
-      Array.from(li.children).forEach((div, index) => {
-        if (index === 0 && div.querySelector('picture')) {
-          div.className = 'imageslider__bg';
-        } else {
-          div.className = 'imageslider__fg';
-        }
-      });
-  
-      ul.append(li);
+        slide.className = `imageslider__slide ${i === 0 ? ' imageslider__slide--active' : ''}`;
+        slide.id = `slide-${i}`;
+
+        Array.from(slide.children).forEach((div, index) => {
+            if (index === 0 && div.querySelector('picture')) {
+                div.className = 'imageslider__bg';
+            } else {
+                div.className = 'imageslider__fg';
+            }
+        });
     });
-  
+
+    
+
     let slideIndex = 0;
-  
+
     function showSlide(n) {
-      Array.from(ul.children).forEach((slide, index) => {
-        if (index === n) {
-          slide.classList.add('imageslider__slide--active');
-        } else {
-          slide.classList.remove('imageslider__slide--active');
-        }
-      });
+        slides.forEach((slide, index) => {
+            if (index === n) {
+                slide.classList.add('imageslider__slide--active');
+            } else {
+                slide.classList.remove('imageslider__slide--active');
+            }
+        });
     }
-  
+
     function nextSlide() {
-      slideIndex = (slideIndex + 1) % slides.length;
-      showSlide(slideIndex);
+        slideIndex = (slideIndex + 1) % slides.length;
+        showSlide(slideIndex);
     }
-  
+
     function prevSlide() {
-      slideIndex = (slideIndex - 1 + slides.length) % slides.length;
-      showSlide(slideIndex);
+        slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+        showSlide(slideIndex);
     }
-  
+
     // Create navigation div
     const navigationDiv = document.createElement('div');
     navigationDiv.className = 'imageslider__navigation';
-  
+
+
     const nextButton = document.createElement('button');
     nextButton.className = 'imageslider__navigation-btn imageslider__navigation-btn--right';
     nextButton.addEventListener('click', nextSlide);
-  
+
     const prevButton = document.createElement('button');
     prevButton.className = 'imageslider__navigation-btn imageslider__navigation-btn--left';
     prevButton.addEventListener('click', prevSlide);
-  
+
+
     navigationDiv.appendChild(prevButton);
     navigationDiv.appendChild(nextButton);
-  
+
     // Append navigation div to block
-    block.textContent = '';
-    block.append(ul);
-    block.append(navigationDiv);
-  
+    block.appendChild(navigationDiv);
+
     // Show the first slide
     showSlide(slideIndex);
-  
+
+
     // Change slide every 5 seconds
     setInterval(nextSlide, 5000);
-  }
+}
